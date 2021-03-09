81a3b8e3812d7101e5d39819935066278cdc57b8
Ivan Pilukshin <slonik150497@gmail.com>
2021-03-09 09:17:34 +0300

    Add project files.

---
+++ HelloWorld_2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+// вызывает функцию std::cout
+void print()
+{
+    // ѕотому что хотим использовать стандартную библиотеку дл€ печати в консоль и
+    // не использовать какие либо другие библиотеки 
+    std::cout << "Hello skillbox!\n";
+
+}
+/*
+* ѕрисваиваем значение переменным и печатаем hello world!
+*/
+int main()
+{
+    int x = 100;
+    3 + 7;
+    int y = x + 100;
+
+    int b = 0;
+    b = b + 2;
+
+    int test;
+    test = 100500;
+
+    int test2 = 1005001;
+
+
+
+    int mult = x * y;
+    int random = 1002;
+   
+    std::cout << random;
+}
